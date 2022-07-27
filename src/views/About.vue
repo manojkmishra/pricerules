157f2eca1e209f4ffda870d5039fbdf618961d60
manojkmishra <manojmishra571980@gmail.com>
2022-07-27 21:22:30 +1000

    img corrected

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
     <p>Higher order function used as below</p>
     <v-img  max-height="50%"  max-width="70%"  :src="imgs.img0"></v-img>
     <p class="mt-4">1. SecondBite</p><p> - Gets a 3 for 2 deal on Classic Ads</p>
-    <v-img  max-height="50%"  max-width="70%"  :src="imgs.img2"></v-img>
+    <v-img  max-height="50%"  max-width="70%"  :src="imgs.img1"></v-img>
     <p class="mt-4">2. Axil Coffee Roasters</p><p> - Gets a discount on Stand out Ads where the price drops to $299.99 per ad</p>
     <v-img  max-height="50%"  max-width="70%"  :src="imgs.img2"></v-img>
     <p class="mt-4">3. MYER</p>
@@ -18,7 +18,7 @@ export default {
    data() {
      return{
        imgs:{img0: require('@/assets/hofn.png'),
-       img1: require('@/assets/axilcofee.png'),
+       img1: require('@/assets/3for2.png'),
        img2: require('@/assets/axilcofee.png'),
        img3: require('@/assets/myer.png')
        }
